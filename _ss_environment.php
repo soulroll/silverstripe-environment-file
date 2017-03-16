79a75b4b56739d8b84318e33f81fbbf9ca94159c
David Malone <david@souldesign.net.nz>
2017-03-16 23:37:32 +1300

    Add files via upload

---
+++ _ss_environment.php
@@ -0,0 +1,24 @@
+<?php
+
+// Database
+define('SS_DATABASE_SERVER','localhost');
+define('SS_DATABASE_USERNAME','root');
+define('SS_DATABASE_PASSWORD','password');
+
+// Creates or connects to the database based on the 'project folder' name
+define('SS_DATABASE_CHOOSE_NAME', true);
+
+// Force email to go to you, no matter what address is specified
+define('SS_SEND_ALL_EMAILS_TO', 'david@souldesign.net.nz');
+
+// What kind of environment is this: dev, test, or live?
+define('SS_ENVIRONMENT_TYPE','dev'); // 'dev/test/live'
+
+// Turn on display_errors if environment is set to 'dev or test'
+if (defined('SS_ENVIRONMENT_TYPE') && SS_ENVIRONMENT_TYPE != 'live') {
+	ini_set('display_errors', 1);
+}
+
+// CMS Admin login defaults
+define('SS_DEFAULT_ADMIN_USERNAME', 'admin');
+define('SS_DEFAULT_ADMIN_PASSWORD', 'password');
